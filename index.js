e5913245a1df8b4566db829ba08424d149cd07e9
ragoutam <93366311+ragoutam@users.noreply.github.com>
2022-02-14 12:47:32 +0530

    more js

--- index.js
+++ index.js
@@ -25,32 +25,63 @@ nextBtn.addEventListener("click", () => {
         nextBtn.style.display = "none";
         cashGivenInput.style.display = "block";
     }
-    else{
+    else {
         showError("Invalid input")
     }
 })
 
 // checkBtn clicked handler
-checkBtn.addEventListener("click",()=>{
+checkBtn.addEventListener("click", () => {
     clearNoOfNotes();
     hideError();
-    let billAmtVal=Number(billAmt.value);
-    let cashGivenVal=Number(cashGiven.value);
+    let billAmtVal = Number(billAmt.value);
+    let cashGivenVal = Number(cashGiven.value);
 
-    
+    if (billAmtVal > 0 && cashGivenVal > 0) {
+        if (!Number.isInteger(cashGivenVal)) {
+            showError("Please enter valid ammount in cash-given field")
+            return;
+        }
+        if (cashGivenVal < billAmtVal) {
+            showError("Given-cash is less than the bill ammount,please enter right ammount")
+            return;
+        }
+        calculateNotes(billAmtVal, cashGivenVal)
 
-
-
-
+    }
+    else {
+        showError("Please enter valid bill ammount and given-cash ammount")
+    }
 })
 
 
+// function to calculate no. of notes to be given
+function calculateNotes(bill,cash){
+    let returnAmt=cash-bill;
+    if (returnAmt<1){
+        showError("Don't return anything!")
+        return; 
+    }
+    changeReturn.style.display="block";
+    for (i=0;i<arrayNoteAmt.length;i++){
+compare(returnAmt,arrayNoteAmt[i],i)
+
+    }
+
+}
 
 
+//compare with currency and post the no. of notes on screen
+function compare(remainder,note,index){
+  if(remainder>=note){
+      let notes=Math.floor(remainder/note);
+      remainder=remainder-notes*note;
+      noOfNotes[index].innerText=`${notes}`
 
+  }
+  return remainder
 
-
-
+}
 
 
 
@@ -59,14 +90,14 @@ function hideError() {
     errorMsg.style.display = "none"
 }
 // function to show invalid input
-function showError(text){
-    errorMsg.style.display="block";
-    errorMsg.innerText=text;
-    changeReturn.style.display="none";
+function showError(text) {
+    errorMsg.style.display = "block";
+    errorMsg.innerText = text;
+    changeReturn.style.display = "none";
 }
 //if check button clicked without refreshing the page, clear the no of notes values on the screen
-function clearNoOfNotes(){
-for(let notes of noOfNotes)
-{
-notes.innerText="";}
+function clearNoOfNotes() {
+    for (let notes of noOfNotes) {
+        notes.innerText = "";
+    }
 }
